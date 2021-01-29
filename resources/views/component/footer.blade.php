4d1e7e32ea7aea5afff8c183449fc42357a34e7b
adityanrlhmd <71908143+adityanrlhmd@users.noreply.github.com>
2021-01-29 08:02:30 +0600

    uas-web

---
+++ resources/views/component/footer.blade.php
@@ -0,0 +1,31 @@
+<section class="dua">
+    <div class="container">
+        <div class="row font-size-03">
+            <div class="col-12 block-hidden-01">
+                <h2 class="font-size-08 font-02 font-weight-bold d-block d-sm-none">Pada ITB Talks #4: Refleksi
+                    2020, Ekspektasi 2021</h2>
+            </div>
+            <div class="col-12 text-center mb-5">
+                <h2
+                    class="font-size-08 font-02 font-weight-bold d-none d-md-block d-lg-block d-xl-block pb-3 text-center">
+                    Pada ITB Talks #4: Refleksi 2020, Ekspektasi 2021</h2>
+            </div>
+            <div class="col-12 col-md-4">
+                <img class="w-100 mb-3" src="https://www.itb.ac.id/images/rektor/Orange.png">
+            </div>
+            <div class="col-12 col-md-8 my-auto">
+                <p>Kita saat ini mendekati penghujung tahun 2020, tahun yang penuh perubahan dan tantangan, yang
+                    menuntut kesabaran, ketabahan dan kebersamaan. Mulai memegang amanah sebagai Rektor ITB pada
+                    bulan Januari 2020, saya memegang prinsip bahwa kepemimpinan ITB merupakan estafet yang bersifat
+                    sinambung dan progresif. Visi tentang ITB pada tahun 2025 adalah menjadi perguruan tinggi dengan
+                    budaya dan tradisi unggul. Visi tersebut saya terjemahkan kedalam Sosok ITB 2025: <em>Globally
+                        Respected, Locally Relevant</em>, melalui kepemimpinan yang bersifat transformasional. <br>
+                </p>
+            </div>
+            <div class="col-12 text-center">
+                <a href="https://www.itb.ac.id/kolom-rektor/sambutan-rektor/pada-itb-talks-4-refleksi-2020-ekspektasi-2021"
+                    class="btn btn-outline-info block-more-03 mt-3">Selengkapnya</a>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/component/header.blade.php
@@ -0,0 +1,59 @@
+<header class="sticky-top">
+    <div class="atas bg-dark">
+        <div class="container d-flex flex-row">
+            <div class="d-flex kiri mr-auto p-2 pr-2 border-right">
+                <img class="mr-2" src="image/flag-en.png" alt="">
+                <img src="https://www.itb.ac.id//themes/itbnew3/images/flag-id.png" alt="">
+            </div>
+            <div class="d-flex kanan flex-row p-2 my-auto">
+                <a class="mr-4 my-auto" href="">Staf</a>
+                <a class="mr-4 my-auto" href="">Mahasiswa</a>
+                <a class="mr-4 my-auto" href="">Alumni</a>
+                <a class="mr-4 my-auto" href="">Mitra</a>
+                <a class="mr-4 my-auto" href="">Pengunjung</a>
+                <a class="mr-4 my-auto" href="">Pers</a>
+                <a class="mr-4 my-auto" href="">My ITB</a>
+                <a class="mr-4 my-auto p-1 text-white" style="border-radius: 5px; background: red;" href="">Covid
+                    19</a>
+                <a class="mr-4 my-auto" href="">Kontak</a>
+                <a class="my-auto" href="">Cari</a>
+            </div>
+        </div>
+    </div>
+    <div class="bawah bg-primary">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg ">
+                <a class="navbar-brand" href="#"><img src="image/logo-itb.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link text-white" href="#">Tentang ITB <span
+                                    class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Penerimaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Penelitian</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Pengabdian</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Penghargaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Multikampus</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
